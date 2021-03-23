d2e026a61412e411cc2e2c7c384c292d58031554
jlmnk12 <https://github.com/jlmnk12/coin2021.git>
2021-03-23 12:55:46 +0900

    메뉴 반복  for,  문자열 str()

--- game.py
+++ game.py
@@ -7,19 +7,3 @@ if a == "1":
     print("싸운다")
 if a == "2":
     print("도망간다")
-
-#밥 먹기
-#메뉴 1, 2, 3
-#메뉴 선택시 음식이름 출력
-
-print("밥을 먹으러간다.")
-print("1~3중에 고르시오")
-menu = input("번호 : ")
-print("고른 번호 : ",menu)
-
-if menu == "1":
-    print("햄버거")
-if menu == "2":
-    print("짜장면")
-if menu == "3":
-    print("피자")
