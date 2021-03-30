2070ac81e5209d1bc37be9273a829045bcaa6e4f
jlmnk12 <skycall1233@gmail.com>
2021-03-30 12:48:39 +0900

    랜덤, 리스트

--- food.py
+++ food.py
@@ -1,17 +1,20 @@
-#밥 먹기
-#메뉴 1, 2, 3
-#메뉴 선택시 음식이름 출력
-## 아래를 전체 반복 ##
-for i in range(1,4):
-    print("밥을 먹으러간다.")
-    print("메뉴는?")
-    print("1.학식 2.분식 3.중식")
-    menu = input(str(i)+".입력 : ")
-    #만약에 사용자의 입력값이 1 이면
-    if menu == "1":
-        print("학식")
-    if menu == "2":
-        print("분식")
-    if menu == "3":
-        print("중식")
-    ## 여기까지 반복 ##
+### 아래 전체를 반복 ###
+import random
+for i in range(1, 4):
+    print("밥무러 가자")
+    print("메뉴는? ")
+    #메뉴 변수
+    menulist = '쫄면', '육계장', '비빔밥'
+    print("1.쫄면 2.육계장 3.비빔밥 4.랜덤")
+    menu = input(str(i) + ".입력: ")
+    #만약에 사용자가 입력값이 1 과 같으면
+    if menu == '1':
+        print("쫄면 먹어라")
+    if menu == '2':
+        print("육계장 먹어라")
+    if menu == '3':
+        print("비빔밥 먹어라")
+    if menu == '4':
+        print(random.choice(menulist))
+    # 랜덤을 고르면 위 메뉴중에서 아무거나 #
+### 여기까지 반복 ###
---
+++ test.py
@@ -0,0 +1,24 @@
+# import random
+# menu = '쫄면', '육계장', '비빔밥', '돈까스'
+# # 메뉴 출력
+# print(menu)
+# # 그중 랜덤 출력
+# print(random.choice(menu))
+
+# a = '1.쫄면'
+# b = "2.육계장"
+# c = "3.비빔밥"
+# d = "4.돈까스"
+
+#메뉴 리스트 만들기
+
+menulist = ['1.쫄면', '2.육계장', '3.비빔밥', '4.돈까스', '5.짜장면']
+print("메뉴 갯수: ", len(menulist))
+for i in range(len(menulist)):
+
+    print(menulist[i])
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
